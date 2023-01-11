b34c66f3185939dd4d39135aaa29d117ba5ef0d3
amit-arya <amit3128arya@gmail.com>
2023-01-11 17:16:54 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const User = require('./models/user');
+const Expense = require('./models/expense');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -22,9 +23,9 @@ app.post('/user/signup', async (req, res) => {
         const password = req.body.password;
 
         const saltrounds = 10;
-        bcrypt.hash(password, saltrounds, async (err, hash)=>{
-        const user = await User.create({ name: name, email: email, password: hash });
-        res.status(201).json({ newUser: user });
+        bcrypt.hash(password, saltrounds, async (err, hash) => {
+            const user = await User.create({ name: name, email: email, password: hash });
+            res.status(201).json({ newUser: user });
         })
     } catch (err) {
         res.status(500).json({ error: err });
@@ -36,24 +37,58 @@ app.post('/get-user', async (req, res) => {
         const email = req.body.email;
         const password = req.body.password;
 
-        const users = await User.findAll({where : {email}});
-        if(users.length > 0){
-            bcrypt.compare(password, users[0].password, (err, result)=>{
-                if(err){
+        const users = await User.findAll({ where: { email } });
+        if (users.length > 0) {
+            bcrypt.compare(password, users[0].password, (err, result) => {
+                if (err) {
                     throw new Error('something went wrong');
                 }
-                if(result === true){
-                    res.status(200).json({success:true, message:"User logged in successfully"})
+                if (result === true) {
+                    res.status(200).json({ success: true, message: "User logged in successfully" })
+                   // res.redirect('http://127.0.0.1:5500/expense/expense.html');
                 }
-                else{
-                    res.status(400).json({success:false, message:"Incorrect password"})
+                else {
+                    res.status(400).json({ success: false, message: "Incorrect password" })
                 }
             })
         }
-        else{
-            res.status(404).json({success:false, message:"User doesn't exist"})
+        else {
+            res.status(404).json({ success: false, message: "User doesn't exist" })
         }
     } catch (err) {
+        res.status(500).json({ error: err });
+    }
+})
+
+app.post('/add-expense', async (req, res) => {
+    try {
+        const amount = req.body.amount;
+        const desc = req.body.desc;
+        const category = req.body.category;
+
+        const expense = await Expense.create({ amount: amount, desc: desc, category: category });
+        res.status(201).json({ newExpense: expense });
+    }
+    catch (err) {
+        res.status(500).json({ error: err });
+    }
+})
+
+app.get('/get-expenses', async (req, res) => {
+    try {
+        const expenses = await Expense.findAll();
+        res.status(201).json({ expenses });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+})
+
+app.delete('/delete-expense/:id', async (req, res)=>{
+    try{
+        const expenseId = req.params.id;
+        await Expense.destroy({where : {id:expenseId}});
+        res.sendStatus(201);
+    } catch(err) {
         res.status(500).json({ error: err });
     }
 })
