936d3b8490e0446e70f6414a5e3e829be4e9d31b
amit-arya <amit3128arya@gmail.com>
2023-01-16 16:49:16 +0530

    Add files via upload

--- premiumController.js
+++ premiumController.js
@@ -1,26 +1,22 @@
 const User = require('../models/user');
 const Expense = require('../models/expense');
+const sequelize = require('../util/database');
 
 const getLeaderBoard = async (req, res)=>{
     try{
-        const users = await User.findAll();
-        const expenses = await Expense.findAll();
-        const userExpenseGroup = {};
-        expenses.forEach((expense) =>{
-            if(userExpenseGroup[expense.userId]){
-                userExpenseGroup[expense.userId] += expense.amount;
-            } else{
-                userExpenseGroup[expense.userId] = expense.amount;
-            }
+        const leaderboardofusers = await User.findAll({
+            attributes:['id', 'name', [sequelize.fn('sum', sequelize.col('expenses.amount')), 'total_expense']],
+            include:[
+                {
+                    model: Expense,
+                    attributes: []
+                }
+            ],
+            group:['user.id'],
+            order:[['total_expense', 'DESC']]
         })
 
-        var userLeaderBoard = [];
-        users.forEach((user)=>{
-            userLeaderBoard.push({name: user.name, total_expense: userExpenseGroup[user.id] || 0})
-        })
-
-        userLeaderBoard.sort((a, b)=> b.total_expense - a.total_expense);
-        res.status(200).json(userLeaderBoard);
+        res.status(200).json(leaderboardofusers);
     } catch(err){
         console.log(err);
         res.status(500).json(err);
