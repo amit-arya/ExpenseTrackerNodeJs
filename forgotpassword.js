65b6c512697ffd8d057f96d094d70178dcc86a77
amit-arya <amit3128arya@gmail.com>
2023-01-18 01:28:11 +0530

    Add files via upload

--- forgotpassword.js
+++ forgotpassword.js
@@ -1,12 +1,15 @@
-async function forgotpassword(e){
-    e.preventDefault();
+const Sequelize = require('sequelize');
 
-    const email = {email: e.target.email.value};
+const sequelize = require('../util/database');
 
-    try{
-        const result = await axios.post("http://localhost:8080/password/forgotpassword", email);
-    } 
-    catch(err){
-        console.log(err);
-    }
-}
+const Forgotpassword = sequelize.define('forgotpassword',{
+    id:{
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true
+    },
+    active: Sequelize.BOOLEAN,
+    expiresby: Sequelize.DATE
+})
+
+module.exports = Forgotpassword;
