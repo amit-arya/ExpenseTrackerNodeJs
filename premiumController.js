f732c001c15c25d91a2a1c2e836bea30e6119ac1
amit-arya <amit3128arya@gmail.com>
2023-01-21 03:03:52 +0530

    Add files via upload

--- premiumController.js
+++ premiumController.js
@@ -2,6 +2,9 @@ const User = require('../models/user');
 const Expense = require('../models/expense');
 const sequelize = require('../util/database');
 
+const Userservices = require('../services/userservices');
+const S3services = require('../services/s3services');
+
 const getLeaderBoard = async (req, res)=>{
     try{
         const leaderboardofusers = await User.findAll({
@@ -23,4 +26,19 @@ const getLeaderBoard = async (req, res)=>{
     }
 }
 
-module.exports = { getLeaderBoard }
+const downloadExpense = async (req, res)=>{
+    try{
+        const expenses = await Userservices.getExpenses(req);
+        const stringifiedExpenses = JSON.stringify(expenses);
+    
+        const userId = req.user.id;
+        const filename = `expense${userId}/${new Date()}.txt`;
+        const fileURL = await S3services.uploadToS3(stringifiedExpenses, filename);
+        res.status(200).json({fileURL, success:true});
+    } catch(err){
+        console.log(err);
+        res.status(500).json({fileURL:'', success:false, Error:err})
+    }
+}
+
+module.exports = { getLeaderBoard, downloadExpense }
