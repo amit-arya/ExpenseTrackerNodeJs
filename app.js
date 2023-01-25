288631345c7a3b5d6617e6fb7cf0a65e03dbf867
amit-arya <amit3128arya@gmail.com>
2023-01-26 01:31:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,102 +1,66 @@
 const path = require('path');
-const User = require('./models/user');
-const Expense = require('./models/expense');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const sequelize = require('./util/database');
 var cors = require('cors');
-const bcrypt = require('bcrypt');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags:'a'});
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/user/signup', async (req, res) => {
-    try {
-        const name = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
+const expenseRoutes = require('./routes/expense-routes');
+const userRoutes = require('./routes/user-routes');
+const purchaseRoutes = require('./routes/purchase-routes');
+const premiumRoutes = require('./routes/premium-routes');
+const passwordRoutes = require('./routes/password-routes');
 
-        const saltrounds = 10;
-        bcrypt.hash(password, saltrounds, async (err, hash) => {
-            const user = await User.create({ name: name, email: email, password: hash });
-            res.status(201).json({ newUser: user });
-        })
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
-})
+const User = require('./models/user');
+const Expense = require('./models/expense');
+const Order = require('./models/order');
+const Forgotpassword = require('./models/forgotpassword');
+const FileURL = require('./models/fileURL');
 
-app.post('/get-user', async (req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
+app.use(expenseRoutes);
+app.use(userRoutes);
+app.use('/purchase', purchaseRoutes);
+app.use('/premium', premiumRoutes);
+app.use('/password', passwordRoutes);
 
-        const users = await User.findAll({ where: { email } });
-        if (users.length > 0) {
-            bcrypt.compare(password, users[0].password, (err, result) => {
-                if (err) {
-                    throw new Error('something went wrong');
-                }
-                if (result === true) {
-                    res.status(200).json({ success: true, message: "User logged in successfully" })
-                   // res.redirect('http://127.0.0.1:5500/expense/expense.html');
-                }
-                else {
-                    res.status(400).json({ success: false, message: "Incorrect password" })
-                }
-            })
-        }
-        else {
-            res.status(404).json({ success: false, message: "User doesn't exist" })
-        }
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
-})
+const dotenv = require('dotenv');
+const { ConfigurationServicePlaceholders } = require('aws-sdk/lib/config_service_placeholders');
+dotenv.config();
 
-app.post('/add-expense', async (req, res) => {
-    try {
-        const amount = req.body.amount;
-        const desc = req.body.desc;
-        const category = req.body.category;
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
-        const expense = await Expense.create({ amount: amount, desc: desc, category: category });
-        res.status(201).json({ newExpense: expense });
-    }
-    catch (err) {
-        res.status(500).json({ error: err });
-    }
-})
+User.hasMany(Order);
+Order.belongsTo(User);
 
-app.get('/get-expenses', async (req, res) => {
-    try {
-        const expenses = await Expense.findAll();
-        res.status(201).json({ expenses });
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
-})
+User.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(User);
 
-app.delete('/delete-expense/:id', async (req, res)=>{
-    try{
-        const expenseId = req.params.id;
-        await Expense.destroy({where : {id:expenseId}});
-        res.sendStatus(201);
-    } catch(err) {
-        res.status(500).json({ error: err });
-    }
-})
+User.hasMany(FileURL);
+FileURL.belongsTo(User);
 
 sequelize
     .sync()
     .then(result => {
-        app.listen(8080);
+        app.set('port', process.env.PORT || 8080);
+       // console.log(`listening at port 8080`);
     })
     .catch(err => {
         console.log(err);
